e1c1856bdbe0de1b8047f2f1c29b65bd0e9663a9
renzhao <renzhao@vpgame.cn>
2023-10-25 15:58:13 +0800

    feat: 添加rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,11 @@
+import typescript from '@rollup/plugin-typescript'
+const config = {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+  },
+  plugins: [typescript()],
+}
+
+export default config
